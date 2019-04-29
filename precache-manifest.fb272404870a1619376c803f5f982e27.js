169e988f4b2f881ec9e73ebef4b9e9c1777b2762
sharitacumm <emandraine@yahoo.com>
2019-04-29 16:10:13 -0400

    Updates

---
+++ precache-manifest.fb272404870a1619376c803f5f982e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "465784663cdbfd4444d5",
+    "url": "/ClickyGame/static/js/main.c84b977f.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ClickyGame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "465784663cdbfd4444d5",
+    "url": "/ClickyGame/static/css/main.3ec930f1.chunk.css"
+  },
+  {
+    "revision": "25497cf8db1e83c99ed356ba6b2fd509",
+    "url": "/ClickyGame/index.html"
+  }
+];
